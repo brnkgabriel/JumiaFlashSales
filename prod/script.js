9f58469fa2fb97e067cec2b547433adc78432efb
brnkgabriel <brnkgabriel@gmail.com>
2019-08-09 10:32:14 +0100

    adapted version

--- prod/script.js
+++ prod/script.js
@@ -7,21 +7,17 @@ Main.prototype.init = function () {
   var sortedSKUS = this.sales.qSort(this.sales.rawSKUs)
   var expandedSKUS = this.sales.expand(sortedSKUS)
   var timeKeys = this.sales.skuTimes(expandedSKUS)
-  // var dayKeys = this.sales.skuDays(expandedSKUS)
   var groupedSKUs = this.sales.groupedSKUs(expandedSKUS, timeKeys, 'time')
   this.sales.buildTimeSKUS(groupedSKUs)
   this.sales.setState(groupedSKUs)
   this.sales.addListeners(groupedSKUs)
-  var markedSKUS = this.sales.markAsSold(groupedSKUs)
+  this.sales.markAsSold(groupedSKUs)
   this.sales.startTimer(groupedSKUs)
-  console.log('markedSKUS', markedSKUS)
-  // console.log('expanded', timeKeys)
 }
 
 var main = new Main()
 main.init()
 
-// **************** POPUP SCRIPT ***************
 var show = document.querySelector('.md-show');
 var mask = document.querySelector('.md-mask');
 var close = document.querySelector('.md-close')
